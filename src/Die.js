35d293dac4fce198ca140df7936d5ec88e217d16
Michal Janowski <michaljanjanowski@gmail.com>
2019-08-24 15:47:00 +0200

    initial setup

---
+++ src/Die.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+class Die extends Component{
+    render(){
+        return(
+            <Die />
+        )
+    }
+}
+
+export default Die
---
+++ src/RollDie.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Die from './Die'
+
+class RollDie extends Component{
+    render(){
+        return(
+            <Die />
+        )
+    }
+}
+
+export default RollDie
